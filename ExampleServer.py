6c651068f07d9b06331e1b12bb78301d50570598
Nicholas Sorenson <soren07@vt.edu>
2015-10-06 19:55:04 -0400

    Added reply functionality.

--- ExampleServer.py
+++ ExampleServer.py
@@ -5,20 +5,42 @@ import riak
 messageStack = Flask(__name__)
 myClient = riak.RiakClient(pb_port=10017, protocol='pbc')
 myBucket = myClient.bucket('message_test')
-count = 0
 
 class SimpleMessage:
-    def __init__(self):
-        global count
+    count = 0
+
+    def __init__(self, increment=True):
         self.sender = "John Doe"
         self.recipient = "To whom it may concern"
         self.body = "Message"
-        self.key = count
-        count += 1
+        self.key = SimpleMessage.count
+        if increment:
+            SimpleMessage.count += 1
 
     def serialize(self):
         return { 'sender': self.sender, 'recipient': self.recipient, 'body': self.body, 'key': self.key }
 
+    @classmethod
+    def deserialize(other, serial):
+        other = SimpleMessage(False)
+        other.sender = serial['sender']
+        other.recipient = serial['recipient']
+        other.body = serial['body']
+        other.key = serial['key']
+        return other
+
+    def reply(self, body):
+        replyString = "\n-------\n|Reply|\n-------\n\n"
+        other = SimpleMessage()
+        other.body = body + replyString + self.body
+        other.sender = self.recipient
+        other.recipient = self.sender
+        return other
+
+    def store(self):
+        serial = self.serialize()
+        riakKey = myBucket.new(str(self.key), data=serial)
+        riakKey.store()
 
 
 @messageStack.route('/message/<int:keyID>', methods=['GET'])
@@ -40,10 +62,20 @@ def create_message():
     newMessage.sender = request.json['sender']
     newMessage.recipient = request.json['recipient']
     newMessage.body = request.json.get('body',"")
-    serial = newMessage.serialize()
-    riakKey = myBucket.new(str(newMessage.key), data=serial)
-    riakKey.store()
-    return jsonify({'message': serial}), 201
+    newMessage.store()
+    return jsonify({'message': newMessage.serialize()}), 201
+
+@messageStack.route('/message/reply', methods=['POST'])
+def reply_message():
+    if not request.json or not ('key' in request.json and 'body' in request.json):
+        abort(400)
+    fetch = myBucket.get(str(request.json['key']))
+    if (fetch.data is None):
+        abort(400)
+    storedMessage = SimpleMessage.deserialize(fetch.data)
+    replyMessage = storedMessage.reply(request.json['body'])
+    replyMessage.store()
+    return jsonify({'message': replyMessage.serialize()}), 201
 
 if __name__ == '__main__':
         messageStack.run(debug=True)
